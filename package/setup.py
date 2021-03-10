1f4bf0fdf5103e61d2091be47a4e6e526c4dc37e
Tomas Babej <tomas@tbabej.com>
2021-03-10 12:38:31 -0500

    package: Add setup.py

---
+++ package/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import os
+from setuptools import find_packages, setup
+
+# Package name
+package_name = "proteinqure_rdkit"
+
+# Specify the dependencies of the package
+requirements = ["numpy"]
+
+PY_MIN_VERSION = os.getenv("PY_MIN_VERSION")
+PY_MAX_VERSION = os.getenv("PY_MAX_VERSION")
+
+# Package setup.
+setup(
+    name=package_name,
+    version="2020.09.5",
+    python_version=f">{PY_MIN_VERSION},<{PY_MAX_VERSION}",
+    install_requires=requirements,
+    long_description=open("README.md").read(),
+    author="ProteinQure team",
+    author_email="team@proteinqure.com",
+    license="BSD-3",
+    packages=find_packages(),
+    package_data={'rdkit': ['*.so', '*/*.so', '*/*/*.so', '*/*/*/*.so']},
+)
